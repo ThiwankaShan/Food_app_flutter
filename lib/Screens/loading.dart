776d25c9d8de40009c66657a12fe41abf2caff86
ThiwankaShan <shane.thiwanka@gmail.com>
2020-10-26 00:04:29 +0530

    added new loading screen

--- lib/Screens/loading.dart
+++ lib/Screens/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingScreen extends StatelessWidget {
   @override
@@ -8,7 +9,16 @@ class LoadingScreen extends StatelessWidget {
       home: Scaffold(
         body: Container(
           alignment: Alignment.center,
-          child: Text('Loading'),
+          child: SpinKitFadingCircle(
+            itemBuilder: (BuildContext context, int index) {
+              return DecoratedBox(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: index.isEven ? Colors.blue : Colors.yellow,
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
